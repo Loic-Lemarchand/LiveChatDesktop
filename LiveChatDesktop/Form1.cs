2170e9c9d5f07bd4409760ce74977819f7caae0d
loiclemarchand <loic.lemarch@gmail.com>
2025-03-05 01:14:19 +0100

    base transparent window opening and video play testing

---
+++ LiveChatDesktop/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using AxWMPLib;
+
+
+namespace LiveChatDesktop
+{
+    public partial class Form1 : Form
+    {
+        private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        private const UInt32 SWP_NOSIZE = 0x0001;
+        private const UInt32 SWP_NOMOVE = 0x0002;
+        private const UInt32 TOPMOST_FLAGS = SWP_NOMOVE | SWP_NOSIZE;
+
+
+
+
+
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.BackColor = Color.White;
+            this.TransparencyKey = Color.White;
+            SetWindowPos(this.Handle, HWND_TOPMOST, 0, 0, 0, 0, TOPMOST_FLAGS);
+            this.FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+
+            axWindowsMediaPlayer1.uiMode = "None";
+            axWindowsMediaPlayer1.windowlessVideo = true;
+
+            //Tests
+            axWindowsMediaPlayer1.URL = @"C:\Users\loic4\Desktop\TirSniper.mp4";
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            
+
+        }
+
+    }
+}
+
